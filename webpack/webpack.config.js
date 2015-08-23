98c490e73febf461a24cd6b2b05fe884bde99d66
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-08-23 11:21:32 +0200

    first commit and basics

---
+++ webpack/webpack.config.js
@@ -0,0 +1,16 @@
+var pkg = require('./package.json');
+
+module.exports = {
+  resolve: {
+    extensions: ['', '.jsx', '.js']
+  },
+    entry: './app.jsx',
+    output: {
+      path: './build',
+      filename: pkg.name + '.js'
+    }
+
+    module: {
+      loaders: [{ test: /\.jsx$/, loader: 'jsx-loader' }]
+    }
+};
