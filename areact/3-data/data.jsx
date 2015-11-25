2c6a76495b9d0c3880067eee684bfef9b6477add
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-25 19:21:23 +0100

    Add Card

--- areact/3-data/data.jsx
+++ areact/3-data/data.jsx
@@ -19,10 +19,16 @@ var Card = React.createClass({
   }
 });
 var Form = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var loginInput = React.findDOMNode(this.refs.login);
+    this.props.addCard(loginInput.value);
+    loginInput.value = '';
+  },
   render: function() {
     return (
-      <form>
-        <input type="text" placeholder="Github Login" />
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Github Login" ref="login" />
           <button>Add</button>
       </form>
    )
@@ -33,13 +39,17 @@ var Main = React.createClass({
   getInitialState: function() {
     return {logins: []};
   },
+  addCard: function(loginToAdd) {
+    this.setState({logins: this.state.logins.concat(loginToAdd)});
+  },
   render: function() {
     var cards = this.state.logins.map(function(login){
       return (<Card login={login} />);
     });
     return (
       <div>
-        <Form />
+        <Form addCard={this.addCard} />
+        {cards}
       </div>
     )
   }
