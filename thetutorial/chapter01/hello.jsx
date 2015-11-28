24a0393ad931247bf19568b6be874164d1c20e42
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-11-28 19:55:45 +0100

    Hello hello again!

---
+++ thetutorial/chapter01/hello.jsx
@@ -0,0 +1,19 @@
+var Hello = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+var Message = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Hello React</h1>
+        <Hello name="Maria" />
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<Message />, document.body);
