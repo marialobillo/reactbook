a31694840803837d23f2194c2c632bcffad7b426
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-03 12:34:59 +0100

    css OK

--- thetutorial/props/props.jsx
+++ thetutorial/props/props.jsx
@@ -2,7 +2,7 @@ var Elemento = React.createClass({
   render: function(){
     return (
       <button className="btn btn-primary">
-        Messages <span class="badge">4</span>
+        Messages <span className="badge">4</span>
       </button>
     );
   }
