1e7bb18630c9a3487b2a1a43e2b109911fe1d652
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-04 13:27:17 +0100

    header and description for thumbnail

--- thetutorial/thumbnail06/thumbnail.jsx
+++ thetutorial/thumbnail06/thumbnail.jsx
@@ -1,4 +1,4 @@
-var Thumbnail = React.createClass({
+var Badge = React.createClass({
   render: function(){
     return (
       <button className="btn btn-primary" type="button">
@@ -8,9 +8,29 @@ var Thumbnail = React.createClass({
   }
 });
 
+var Thumbnail = React.createClass({
+    render: function(){
+      return (
+        <div className="thumbnail">
+          <img src={this.props.imageUrl} />
+          <div className="caption">
+            <h3>{this.props.header}</h3>
+            <p>{this.props.description}</p>
+            <p>
+              <a href="#" className="btn btn-primary" role="button">Button</a>
+              <a href="#" className="btn btn-default" role="button">Button</a>
+            </p>
+          </div>
+        </div>
+      );
+    }
+});
+
 var options = {
   title: 'Sent',
-  number: 12
+  number: 12,
+  header: 'Learn React',
+  description: 'React is fantastic new library for making fast, dynamic pages.'
 }
 
 var element = React.createElement(Thumbnail, options);
