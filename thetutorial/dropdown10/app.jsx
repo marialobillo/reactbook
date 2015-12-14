d70cc0a16dce8a893d37ab89edb9575a8e67d5cd
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-14 11:50:35 +0100

    dropdown menu

---
+++ thetutorial/dropdown10/app.jsx
@@ -0,0 +1,63 @@
+
+var ListItem = React.createClass({
+  render: function(){
+    return (
+      <li><a href="">{this.props.item}</a></li>
+    );
+  }
+});
+
+var Badge = React.createClass({
+  render: function(){
+    return (
+      <div className="btn-group">
+        <button onClick={this.props.whenClicked} type="button"
+          className="btn btn-success dropdown-toggle" >
+          {this.props.title}
+          <span className="badge">{this.props.number}</span>
+        </button>
+
+      </div>
+    );
+  }
+});
+
+
+
+
+
+var Dropdown = React.createClass({
+  handleClick: function(){
+    alert('Hello from dropdown')
+  },
+  render: function(){
+    var list = this.props.items.map(function(item){
+      return (
+        <ListItem item={item} />
+      );
+    });
+    return (
+      <div className="btn-group">
+        <Badge whenClicked={this.handleClick} className="btn-success" title={this.props.title}
+          subTitleClassName="caret" subTitle="29"/>
+        <ul className="dropdown-menu">
+          {list}
+        </ul>
+      </div>
+    );
+  }
+});
+
+var options = {
+  title: 'Choose a desert',
+  items: [
+    'Apple Pie',
+    'Peach Cobbler',
+    'Coconut Cream Pie'
+  ]
+};
+
+
+var element = React.createElement(Dropdown, options);
+
+ReactDOM.render(element, document.querySelector('.target'));
