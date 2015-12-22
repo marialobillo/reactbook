c36a1cb07fd56ff30f75621ac28f2577af8b7736
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-22 13:10:43 +0100

    add Header

--- thetutorial/ReactStarter/src/app.jsx
+++ thetutorial/ReactStarter/src/app.jsx
@@ -1,20 +1,25 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var Header = require('./header');
 var rootUrl = 'https://crackling-heat-1593.firebaseio.com';
 
 var App = React.createClass({
   mixins: [ ReactFire ],
   componentWillMount: function() {
     this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
-    // this.state.items => {}
   },
   render: function() {
-    console.log(this.state);
-    
-    return (<h1 className="red">
-      Hello, React!!
-    </h1>);
+    return (
+      <div className="row">
+        <div className="panel panel-default">
+          <div className="col-md-8 col-md-offset-2">
+            <h2 className="text-center">To-Do List</h2>
+            <Header />
+          </div>
+        </div>
+      </div>
+    );
   }
 });
 
