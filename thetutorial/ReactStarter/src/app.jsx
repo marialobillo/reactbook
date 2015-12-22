caaaba8533fb738d99122024fde72d2dcd0eb984
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-22 12:52:07 +0100

    set the app.jsx

--- thetutorial/ReactStarter/src/app.jsx
+++ thetutorial/ReactStarter/src/app.jsx
@@ -1,9 +1,17 @@
 var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var rootUrl = 'https://crackling-heat-1593.firebaseio.com';
 
 var App = React.createClass({
+  mixins: [ ReactFire ],
+  componentWillMount: function() {
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    // this.state.items => {}
+  },
   render: function() {
+    console.log(this.state);
+    
     return (<h1 className="red">
       Hello, React!!
     </h1>);
