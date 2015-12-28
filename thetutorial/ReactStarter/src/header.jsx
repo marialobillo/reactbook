cd29f44fe5aa75d31a14d2c7332d614a29c5cedc
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-28 13:09:55 +0100

    event state text

--- thetutorial/ReactStarter/src/header.jsx
+++ thetutorial/ReactStarter/src/header.jsx
@@ -1,14 +1,35 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return (
+      text: ''
+    );
+  },
   render: function(){
     return (
       <div className="input-group">
-        <input type="text" className="form-control"/>
+        <input
+          value={this.state.text}
+          onChange={this.handleInputChange}
+          type="text"
+          className="form-control"/>
         <span className="input-group-btn">
-          <button className="btn btn-danger" type="button">Add</button>
+          <button
+            onClick={this.handleClick}
+            className="btn btn-danger"
+            type="button">
+            Add
+          </button>
         </span>
+        {this.state.text}
       </div>
     );
+  },
+  handleClick: function() {
+    //Send value of text input to Firebase
+  },
+  handleInputChange: function(){
+    this.setState({text: event.target.value});
   }
 });
